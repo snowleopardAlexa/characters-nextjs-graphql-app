6c153a180a6cdb867d0626f9afb8e7c7baf74dcb
snowleopardAlexa <alexaslomski@gmail.com>
2022-05-31 11:28:08 -0400

    finish characterss comp, map over characters, return properties

--- components/Characters.jsx
+++ components/Characters.jsx
@@ -7,6 +7,9 @@ const Characters = ({ characters }) => {
           return(
            <div key={character.id}>
                <Image src={character.image} width={300} height={300} alt="image" />
+               <h1>{character.name}</h1>
+               <p>{character.origin.name}</p>
+               <p>{character.location.name}</p>
            </div>
           )
           })}
